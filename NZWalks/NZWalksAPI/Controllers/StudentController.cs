7763f5686d66792253dbd13c7b5a333ce2be0dfb
Miebi <User@DESKTOP-1EK6ASL>
2023-11-06 20:55:09 -0800

    update

---
+++ NZWalks/NZWalksAPI/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAllStudents")]
+        public IActionResult GetAllStudents()
+        {
+            List<string> names = new List<string>()
+            {
+                   
+            };
+            if (names.Count is not 0)
+            {
+                return Ok(names);
+            }
+            else
+            {
+                return BadRequest();
+            }   
+
+        }
+    }
+}
